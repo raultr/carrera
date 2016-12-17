2ab02c0a87c4df2a72a15bae02829470bc5abc15
raultr <raultr@gmail.com>
2016-12-16 22:56:40 -0600

    poder exportar a excel

--- registros/admin.py
+++ registros/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from import_export.widgets import ForeignKeyWidget
+from import_export.admin import ImportExportMixin,ImportExportModelAdmin,ImportExportActionModelAdmin,ExportMixin,ExportActionModelAdmin
+from import_export import fields,resources
+from municipios.models import Municipio
 from .models import Registro
 					
-class RegistroAdmin(admin.ModelAdmin):
-	list_display =('id','paterno','materno','nombre','dia','mes','anio','genero','cp','estado','municipio','email','telefono','alergia','duatlon','ciclista','email_ciclista','numero')
+class RegistroAdmin(resources.ModelResource):
+	nmunicipio = fields.Field(column_name='nmunicipio', attribute='municipio', widget=ForeignKeyWidget(Municipio, 'municipio')) 
+
+	class Meta: 
+		model = Registro
+		fields =('id','numero','paterno','materno','nombre','dia','mes','anio','genero','cp','estado','nmunicipio','email','telefono','alergia','duatlon','ciclista','email_ciclista',)
+		export_order = ('numero','id','paterno','materno','nombre','dia','mes','anio','genero','cp','estado','nmunicipio','email','telefono','alergia','duatlon','ciclista','email_ciclista',)
+
+	list_display =('id','numero','paterno','materno','nombre','dia','mes','anio','genero','cp','estado','municipio','email','telefono','alergia','duatlon','ciclista','email_ciclista',)
 
 	search_fields = ('paterno','materno','nombre','numero') # Campos por los que se puede buscar, si son campos foraneos se usa campo__nomcampoforaneo
 
-admin.site.register(Registro,RegistroAdmin)
+class RegistroAdminModel(ImportExportActionModelAdmin):
+	resource_class = RegistroAdmin
+	list_display =('id','numero','paterno','materno','nombre','dia','mes','anio','genero','cp','estado','municipio','email','telefono','alergia','duatlon','ciclista','email_ciclista',)
 
-
+admin.site.register(Registro,RegistroAdminModel)
